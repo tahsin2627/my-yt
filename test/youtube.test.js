04578d76e6d1cc980eebdc67f6ae1bb9cc08020e
Christian Fei <crifei93@gmail.com>
2025-01-21 17:58:05 +0100

    use api chat/completions for summary

---
+++ test/youtube.test.js
@@ -0,0 +1,18 @@
+import { test } from 'node:test'
+import assert from 'assert';
+import { getVideosFor } from '../lib/youtube.js'
+
+test('gets videos for channel', async () => {
+  const videos = await getVideosFor('veritasium')
+  const video = videos[0]
+  assert.ok(video.channelName)
+  assert.ok(video.title)
+  assert.ok(video.url)
+  assert.ok(video.thumbnail)
+  assert.ok(video.description)
+  assert.ok(video.id)
+  assert.ok(video.publishedTime)
+  assert.ok(video.viewCount)
+  assert.ok(video.duration)
+  assert.strictEqual(videos.length, 30)
+})
