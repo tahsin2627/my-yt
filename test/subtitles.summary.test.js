c43230c404cb7988cf800d958d04d9ae51597e61
Christian Fei <crifei93@gmail.com>
2025-03-17 12:25:56 +0100

    feat: improve converting subtitles to txt, remove duplicate lines

--- test/subtitles.summary.test.js
+++ test/subtitles.summary.test.js
@@ -5,21 +5,38 @@ import { cleanTranscript } from '../lib/subtitles-summary.js'
 test('cleans transcript', () => {
   const transcript = `
 1
-00:00:01,240 --> 00:00:06,879
-<font color="white" size=".72c">when we are about to do something</font>
+00:00:01,240 --> 00:00:04,630
+
+Open ai writes to the
 
 2
-00:00:04,160 --> 00:00:10,519
-<font color="white" size=".72c">new</font>
+00:00:04,630 --> 00:00:04,640
+Open ai writes to the
+ 
 
 3
-00:00:06,879 --> 00:00:14,080
-<font color="white" size=".72c">Eh after finishing</font>
+00:00:04,640 --> 00:00:09,110
+Open ai writes to the
+US administration their position Well it is
+
+4
+00:00:09,110 --> 00:00:09,120
+US administration their position Well it is
+ 
+
+5
+00:00:09,120 --> 00:00:12,509
+US administration their position Well it is
+necessary that it be free from copyright from
+
+6
+00:00:12,509 --> 00:00:12,519
+necessary that it be free from copyright from
   `.trim()
   const expectedCleanTranscript = `
-when we are about to do something
-new
-Eh after finishing
+Open ai writes to the
+US administration their position Well it is
+necessary that it be free from copyright from
   `.trim()
   
   assert.strictEqual(cleanTranscript(transcript), expectedCleanTranscript)
