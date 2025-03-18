f422e85b6fe60cc4bfec3b5295c3049922df766d
Christian Fei <crifei93@gmail.com>
2025-03-18 11:24:18 +0100

    feat: get single video information

--- test/youtube.test.js
+++ test/youtube.test.js
@@ -1,6 +1,6 @@
 import { test } from 'node:test'
 import assert from 'assert';
-import { getVideosFor } from '../lib/youtube.js'
+import { getVideosFor, getVideo } from '../lib/youtube.js'
 
 test('gets videos for channel', async () => {
   const videos = await getVideosFor('veritasium')
@@ -15,4 +15,18 @@ test('gets videos for channel', async () => {
   assert.ok(video.viewCount)
   assert.ok(video.duration)
   assert.ok(videos.length > 0)
+})
+
+test('gets single video', async () => {
+  // https://www.youtube.com/watch?v=qJZ1Ez28C-A
+  const video = await getVideo('qJZ1Ez28C-A')
+  assert.equal(video.channelName, '@veritasium')
+  assert.equal(video.title, 'Something Strange Happens When You Trust Quantum Mechanics')
+  assert.equal(video.url, 'https://www.youtube.com/watch?v=qJZ1Ez28C-A')
+  assert.equal(video.thumbnail, 'https://img.youtube.com/vi/qJZ1Ez28C-A/mq2.jpg')
+  assert.ok(video.description, '')
+  assert.equal(video.id, 'qJZ1Ez28C-A')
+  assert.equal(video.publishedTime, '2025-03-05')
+  assert.ok(video.viewCount)
+  assert.equal(video.duration, '33:36')
 })
