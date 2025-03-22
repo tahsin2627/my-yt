10381796243f06f971575e15d947ac0d48711c62
Christian Fei <crifei93@gmail.com>
2025-03-22 20:32:55 +0100

    chore: cleanup

--- client/components/empty-state.js
+++ client/components/empty-state.js
@@ -3,14 +3,17 @@ class EmptyState extends HTMLElement {
     super()
   }
   connectedCallback () {
+    this.classList.add('flex')
+    this.classList.add('space-between')
+    this.classList.add('direction-col')
+    this.classList.add('user-select-none')
     this.render()
   }
   render () {
-    /* html for empty app state */
     this.innerHTML = /*html*/`
       <h1>Nothing to show...</h1>
       <p>Add YouTube channels you want to track in <a href="/settings">/settings</a></p>
-      <div class="big-sad-emoji"></div>
+      <div class="extra-big">🙈</div>
     `
   }
 }
