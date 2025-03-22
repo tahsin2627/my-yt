afdbfec1de43ff4ce365768eca345683e7fc5a87
Christian Fei <crifei93@gmail.com>
2025-03-22 17:22:06 +0100

    style: improve empty state

--- client/components/empty-state.js
+++ client/components/empty-state.js
@@ -18,7 +18,7 @@ class EmptyState extends HTMLElement {
     /* html for empty app state */
     this.innerHTML = /*html*/`
       <h1>Nothing to show...</h1>
-      <p>Add your YouTube subscriptions in the <a href="/settings">Settings</a></p>
+      <p>Add YouTube channels you want to track in <a href="/settings">/settings</a></p>
       <div class="big-sad-emoji"></div>
     `
   }
