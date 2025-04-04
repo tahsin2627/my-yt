0fcb63604957152a135fe672c749acf7d3e4fb7f
Christian Fei <crifei93@gmail.com>
2025-04-04 10:31:03 +0200

    fix: manage-disk-space-form html

--- client/components/manage-disk-space-form.js
+++ client/components/manage-disk-space-form.js
@@ -33,18 +33,18 @@ class ManageDiskSpaceForm extends HTMLElement {
             <button id="reclaim-disk-space" type="submit">Delete videos</button>
           </div>
         </div>
-        <div><small id="disk-usage">Currently <span id="disk-space-used"></span> of disk space used</small></div>
+        <div><small id="disk-usage"></small></div>
       </form>
     `
   }
 
   updateDiskSpaceUsed() {
     const onlyIgnored = this.querySelector('#delete-only-ignored').checked
-    const $diskSpaceUsed = this.querySelector('#disk-space-used')
+    const $diskUsage = this.querySelector('#disk-usage')
     fetch(onlyIgnored ? '/api/disk-usage?onlyIgnored=true' : '/api/disk-usage')
     .then(response => response.text())
     .then((diskSpaceUsed) => {
-      $diskSpaceUsed.innerText = diskSpaceUsed
+      $diskUsage.innerHTML = /*html*/`Currently <span id="disk-space-used">${diskSpaceUsed}</span> of disk space used`
     })
     .catch(err => console.error(err))
   }
