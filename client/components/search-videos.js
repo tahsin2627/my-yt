f4f9507aec37c585dfb7c9afc39f0ec21f76d428
Christian Fei <crifei93@gmail.com>
2025-04-20 13:30:18 +0200

    feat: show excluded videos in search

--- client/components/search-videos.js
+++ client/components/search-videos.js
@@ -18,20 +18,28 @@ class SearchVideos extends HTMLElement {
     this.unregisterEvents()
   }
   registerEvents () {
-    this.querySelector('input').addEventListener('input', this.searchHandler.bind(this))
+    this.querySelector('#search').addEventListener('input', this.searchHandler.bind(this))
+    this.querySelector('#with-excluded').addEventListener('change', this.searchHandler.bind(this))
   }
   unregisterEvents () {
-    this.querySelector('input').removeEventListener('input', this.searchHandler.bind(this))
+    this.querySelector('#search').removeEventListener('input', this.searchHandler.bind(this))
+    this.querySelector('#with-excluded').removeEventListener('change', this.searchHandler.bind(this))
   }
   render () {
-    this.innerHTML = /*html*/`<input type="search" incremental="incremental" id="search" placeholder="🔍 Search videos or paste video url" autofocus>`
+    this.innerHTML = /*html*/`
+      <input type="search" incremental="incremental" id="search" placeholder="🔍 Search videos or paste video url" autofocus>
+      <div>
+        <label for="with-excluded">Show excluded videos</label>
+        <input type="checkbox" id="with-excluded"/>
+      </div>
+    `
   }
   searchHandler (event) {
     event.preventDefault()
-    console.log('searchHandler')
-    let searchTerm = event.target.value.trim()
-    if (this.previousSearchTerm === searchTerm) return
-    this.previousSearchTerm = searchTerm
+    const $search = this.querySelector('#search')
+    let searchTerm = $search.value.trim()
+    if (this.previousSearchTerm === searchTerm && event.target === $search) return
+    if (event.target === $search) this.previousSearchTerm = searchTerm
 
     const $status = document.querySelector('#filter-results-status')
     $status.innerText = ''
@@ -43,14 +51,15 @@ class SearchVideos extends HTMLElement {
         body: JSON.stringify({ id: searchTerm, external: true }),
       })
       .catch((error) => console.error('Error starting download:', error))
-      event.target.value = ''
+      $search.value = ''
       return
     }
 
     searchTerm = searchTerm.toLowerCase()
     document.body.classList.add('searching')
+    let withExcluded = this.querySelector('#with-excluded').checked
 
-    fetch(`/api/videos?filter=${encodeURIComponent(searchTerm)}`)
+    fetch(`/api/videos?filter=${encodeURIComponent(searchTerm)}${withExcluded ? '&withExcluded=true' : ''}`)
     .then(res => res.json())
     .then((videos) => {
       const $videosContainer = document.querySelector('.main-videos-container')
