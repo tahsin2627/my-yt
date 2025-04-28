25d453049561d6f51c897d265a363c925e6f540c
Christian Fei <crifei93@gmail.com>
2025-04-28 16:29:39 +0200

    chore: cleanup server

--- test/llm/index.test.js
+++ test/llm/index.test.js
@@ -1,4 +1,4 @@
-import { test, beforeEach, afterEach } from 'node:test'
+import { test, beforeEach } from 'node:test'
 import assert from 'node:assert'
 import { MockAgent, setGlobalDispatcher } from 'undici'
 
