2062674a49bcc39c87dc9ef82902f6e86bf41c4e
Christian Fei <crifei93@gmail.com>
2025-05-19 15:48:42 +0200

    feat: log Memory usage

--- index.js
+++ index.js
@@ -13,4 +13,11 @@ if (import.meta.url.endsWith('index.js')) {
   main({
     port: +process.env.PORT || 3000
   })
+  logMemoryUsage()
+  setInterval(logMemoryUsage, 5 * 60 * 1000)
+}
+
+function logMemoryUsage () {
+  const memUsage = process.memoryUsage()
+  console.log(`[memory] rss: ${Math.round(memUsage.rss / 1024 / 1024)} MB, heapTotal: ${Math.round(memUsage.heapTotal / 1024 / 1024)} MB, heapUsed: ${Math.round(memUsage.heapUsed / 1024 / 1024)} MB, external: ${Math.round(memUsage.external / 1024 / 1024)} MB`)
 }
