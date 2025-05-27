ca4fbf6b43d83969eedb24b2609c7a9c33d55c37
Christian Fei <crifei93@gmail.com>
2025-05-27 19:45:12 +0200

    fix: callback when summarizing without error

--- server/router/api.js
+++ server/router/api.js
@@ -137,12 +137,8 @@ async function summarizeVideoHandler (req, res, repo, connections = [], state = 
   state.summarizing = state.summarizing || {}
   state.summarizing[id] = { lines: [] }
 
-  summarizeVideo(id, repo, llmSettings, (err, line) => {
-    if (err) {
-      broadcastSSE(JSON.stringify({ type: 'download-log-line', line: `Error summarizing video ${id}: ${err.message}` }), connections)
-    } else {
-      broadcastSSE(JSON.stringify({ type: 'download-log-line', line }), connections)
-    }
+  summarizeVideo(id, repo, llmSettings, (line) => {
+    broadcastSSE(JSON.stringify({ type: 'download-log-line', line }), connections)
   })
     .then(({ summary, transcript }) =>
       broadcastSSE(JSON.stringify({ type: 'summary', summary, transcript, videoId: id }), connections))
